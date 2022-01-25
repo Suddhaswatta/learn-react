3d1b5dcde555dc1f745fd6a6bfcdbab8f46085c6
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-01-25 17:13:38 +0530

    Card system

--- rest-axios-app/src/components/image/ImageDetails.js
+++ rest-axios-app/src/components/image/ImageDetails.js
@@ -3,7 +3,15 @@ import { Component } from "react";
 class ImageDetails extends Component {
   render() {
     const { urls, id, alt_description } = this.props.image;
-    return <img src={urls.regular} key={id} alt={alt_description}></img>;
+    return (
+      <div className="card" key={id} style={{width: '20px'}}>
+        <img
+          src={urls.regular}
+          key={id}
+          alt={alt_description}
+        ></img>
+      </div>
+    );
   }
 }
 
